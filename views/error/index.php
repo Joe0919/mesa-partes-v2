94e27a749133fd49792870e1a5ebbb95b50bcb54
Joel Llallihuaman Giraldo <joel09llalli@gmail.com>
2024-10-20 12:38:59 -0500

    Estilizar Ventana 404

--- views/error/index.php
+++ views/error/index.php
@@ -1,13 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
-    <title>Pagina no encontrada</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= media() ?>/templates/AdminLTE/plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?= media() ?>/icons/ionicons.css">
+    <!-- Feather Icons -->
+    <link rel="stylesheet" href="<?= media() ?>/icons/feather.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= media() ?>/templates/AdminLTE/dist/css/adminlte.min.css">
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Sweet Alert -->
+    <link rel="stylesheet" href="<?= media() ?>/templates/AdminLTE/plugins/sweetalert2/sweetalert2.min.css">
+    <!-- Estilos principales -->
+    <link rel="stylesheet" href="<?= media() ?>/css/style.css">
+
+    <link rel="icon" type="image/png" href="<?= media() ?>/images/logo.png">
+    <title>Pagina No Encontrada</title>
+
 </head>
 
-<body>
-    <h1>Pagina no encontrada</h1>
+<body class="body">
+
+    <div class="error-container">
+        <div class="container-xl bg-color-gray h-100 d-flex align-items-center justify-content-center">
+            <div class="text-center ">
+                <!-- Puedes reemplazar este link con tu propia imagen de señal de tráfico -->
+                <img src="<?= media() ?>/images/404.webp" alt="404">
+                <h2 class="mt-2 text-bold">Página No Encontrada</h2>
+                <p>Lo sentimos, la página que estás buscando no existe. Si cree que algo no funciona, informe el problema.</p>
+                <p>Puede intentar visitar otros enlaces:</p>
+                <div>
+                    <a href="<?= base_url() ?>/" class="btn bg-gradient-primary mt-1">Ir a Inicio</a>
+                    <a href="<?= base_url() ?>/registro-tramite" class="btn bg-gradient-success mt-1">Registrar Trámite</a>
+                    <a href="<?= base_url() ?>/seguimiento" class="btn bg-gradient-warning mt-1">Seguimiento</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap 4 -->
+    <script src="<?= media() ?>/templates/AdminLTE/plugins/bootstrap5/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
